0a07ba899df840fc5bf94425871d2a65c82a0716
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-14 20:01:13 -0500

    homepage

---
+++ templates/content-page-home.php
@@ -0,0 +1,40 @@
+<!-- /*-------- HEADER --------*/ -->
+
+<!-- /*-------- EMAIL SIGNUP --------*/ -->
+
+<div class="row text-right">
+  Email Signup
+</div>
+
+<!-- /*-------- HOME SLIDER VIEW --------*/ -->
+
+<div class="row">
+  SLider
+</div>
+
+<div class="row">
+  Mini Nav
+</div>
+
+<?php echo do_shortcode( "[wpv-view name='banner']"); ?>
+
+<!-- /*-------- HOME BIO VIEW --------*/ -->
+
+<div class="row">
+  <div class="col-sm-6 home-player">
+    Featured Track
+  </div>
+  <div class="col-sm-6 home-bio">
+    <div class="row">
+      Excerpt Title
+    </div>
+    <div class="row">
+      Excerpt
+    </div>
+    <div class="row">
+      Link To Bio Page
+    </div>
+  </div>
+</div>
+
+<!-- /*-------- FOOTER --------*/ -->
