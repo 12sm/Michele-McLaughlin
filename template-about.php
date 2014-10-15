0ec6e084bd5a1b591e2e476a6773a742a0dd9798
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-15 14:44:28 -0500

    header no

--- template-about.php
+++ template-about.php
@@ -4,5 +4,4 @@ Template Name: About Template
 */
 ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page-about'); ?>
--- template-concerts.php
+++ template-concerts.php
@@ -4,5 +4,4 @@ Template Name: Concerts Template
 */
 ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page-concerts'); ?>
