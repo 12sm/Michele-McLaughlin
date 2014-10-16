18dfa6eec986faf1e283597859000b8074eb5aed
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-15 20:57:39 -0500

    banners

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,5 @@
-<div class="row banners container page-header">
-  <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+<div class="row container page-header">
+  <div class="banners col-xs-12 col-sm-8 col-sm-offset-2">
     <h1 class="bandit">
       <?php echo roots_title(); ?>
     </h1>
