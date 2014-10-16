f87b3b3c40d5997ee83c60caa3efb606fbd691dc
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-16 11:36:13 -0500

    fonts

--- templates/head.php
+++ templates/head.php
@@ -25,5 +25,7 @@
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+  <script src="//use.typekit.net/ric0jma.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
   <link rel="shortcut icon" href="<?php echo of_get_option('favicon_upload'); ?>" />
 </head>
