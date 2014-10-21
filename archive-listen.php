6cf224cb33ed2f8fe23761cc3a4d224914505b3a
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-21 14:00:45 -0500

    song test

--- archive-listen.php
+++ archive-listen.php
@@ -2,9 +2,7 @@
 
 <!-- /*-------- EMAIL SIGNUP --------*/ -->
 
-<div class="row text-right">
-  Email Signup
-</div>
+<?php echo do_shortcode("[wpv-view name='email-submit']"); ?>
 
 <!-- /*-------- LISTEN BANNER --------*/ -->
 
@@ -12,30 +10,6 @@
 
 <!-- /*-------- ALBUM VIEW --------*/ -->
 
-[wpv-loop-start]
-  <div class="row bg-div">
-    <div class="col-xs-12">
-      <div class="row">
-        Album Title
-      </div>
-      <div class="row">
-        <div class="col-sm-4">
-          <div class="bg-div">
-            Album Art
-          </div>
-        </div>
-        <div class="col-sm-8">
-          <div class="row">
-            <div class="col-sm-6 double-stack">
-              [wpv-loop-start]
-                Track View
-              [wpv-loop-end]
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-[wpv-loop-end]
+<?php echo do_shortcode("[wpv-view name='songs']"); ?>
 
 <!-- /*-------- FOOTER --------*/ -->
