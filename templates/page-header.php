2f7fa7ab67f5fdaa515891ef60b36b0a1f6b804a
Sarah <Sarah@12southmusic.com>
2014-10-27 17:08:32 -0500

    with a max wrap

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,7 +1,9 @@
-<div class="row page-header">
-  <div class="banners col-md-12 col-lg-12">
-    <h1 class="bandit">
-      <?php echo roots_title(); ?>
-    </h1>
+<div class="max-wrap">
+  <div class="row page-header">
+    <div class="banners col-md-12 col-lg-12">
+      <h1 class="bandit">
+        <?php echo roots_title(); ?>
+      </h1>
+    </div>
   </div>
 </div>
