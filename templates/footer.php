3b51148a6774a951a4f79c1073bfeb09a7c9a2ac
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-10-27 17:59:24 -0500

    footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,70 +1,72 @@
 <footer class="content-info container-fluid" role="contentinfo">
   <div class="row imgLiquidFill imgLiquid">
     <img src="../wp-content/themes/michele/assets/img/footer-bg.png">
-    <div class="row mininav">
-      <div class="col-xs-2 col-xs-offset-3">
-        <a href="/store">
-          <div class="mini">
-            <img src"../assets/img/bullet-footer.png">
-            <h4>
-              BUY MUSIC
-            </h4>
-          </div>
-        </a>
-      </div>
-      <div class="col-xs-2">
-        <a href="/concerts">
-          <div class="mini">
-            <img src"http://michelemclaughlinmusic.12southdev.com/wp-content/themes/michele/assets/img/bullet-footer.png">
-            <h4>
-              CONCERTS
-            </h4>
-          </div>
-        </a>
-      </div>
-      <div class="col-xs-2">
-        <a href="/booking">
-          <div class="mini">
-            <img src"http://michelemclaughlinmusic.12southdev.com/wp-content/themes/michele/assets/img/bullet-footer.png">
-            <h4>
-              BOOK A SHOW
-            </h4>
-          </div>
-        </a>
-      </div>
-    </div>
-    <div class="row">
-      Main Nav
-    </div>
-    <div class="row">
-      Email
-    </div>
-    <div class="row">
-      Socials
-    </div>
-    <div class="row">
-      <div class="col-lg-12">
-
-        <div class="row sidebar-footer">
-          <?php dynamic_sidebar( 'sidebar-footer'); ?>
-        </div>
-
-        <div class="row footer-nav">
-          <?php if (has_nav_menu( 'footer_navigation')) : wp_nav_menu(array( 'theme_location'=>'footer_navigation', 'menu_class' => '')); endif; ?>
-        </div>
-
-        <div id="social" class="row social-nav">
-          <?php if (has_nav_menu( 'social_navigation')) : wp_nav_menu(array( 'theme_location'=>'social_navigation', 'menu_class' => '')); endif; ?>
-        </div>
-
-        <p class="credits">&copy;
-          <?php echo date('Y'); ?>
-          MICHELE MCLAUGHLIN. ALL RIGHTS RESERVED.
-          <a href="http://12southmusic.com/" target="_blank">
-            BUILT BY 12SM.
+    <div class="col-xs-12">
+      <div class="row mininav">
+        <div class="col-xs-2 col-xs-offset-3">
+          <a href="/store">
+            <div class="mini">
+              <img src"../assets/img/bullet-footer.png">
+              <h4>
+                BUY MUSIC
+              </h4>
+            </div>
           </a>
-        </p>
+        </div>
+        <div class="col-xs-2">
+          <a href="/concerts">
+            <div class="mini">
+              <img src"http://michelemclaughlinmusic.12southdev.com/wp-content/themes/michele/assets/img/bullet-footer.png">
+              <h4>
+                CONCERTS
+              </h4>
+            </div>
+          </a>
+        </div>
+        <div class="col-xs-2">
+          <a href="/booking">
+            <div class="mini">
+              <img src"http://michelemclaughlinmusic.12southdev.com/wp-content/themes/michele/assets/img/bullet-footer.png">
+              <h4>
+                BOOK A SHOW
+              </h4>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        Main Nav
+      </div>
+      <div class="row">
+        Email
+      </div>
+      <div class="row">
+        Socials
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
 
+          <div class="row sidebar-footer">
+            <?php dynamic_sidebar( 'sidebar-footer'); ?>
+          </div>
+
+          <div class="row footer-nav">
+            <?php if (has_nav_menu( 'footer_navigation')) : wp_nav_menu(array( 'theme_location'=>'footer_navigation', 'menu_class' => '')); endif; ?>
+          </div>
+
+          <div id="social" class="row social-nav">
+            <?php if (has_nav_menu( 'social_navigation')) : wp_nav_menu(array( 'theme_location'=>'social_navigation', 'menu_class' => '')); endif; ?>
+          </div>
+
+          <p class="credits">&copy;
+            <?php echo date('Y'); ?>
+            MICHELE MCLAUGHLIN. ALL RIGHTS RESERVED.
+            <a href="http://12southmusic.com/" target="_blank">
+              BUILT BY 12SM.
+            </a>
+          </p>
+
+        </div>
       </div>
     </div>
   </div>
