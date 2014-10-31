572e8571eb8a2c43286cddb5040620e7044599ca
Ian Noyes <ian@12southmusic.com>
2014-10-31 17:52:02 -0500

    Revert "Revert "copy cat""
    This reverts commit 7bf9b94d2710e89f5163460609cce8445134c722.

--- woocommerce/taxonomy-product_cat.php
+++ woocommerce/taxonomy-product_cat.php
@@ -9,9 +9,26 @@
  * @version     1.6.4
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-  exit; // Exit if accessed directly
-}
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-echo "taxonomy-product.php";
-wc_get_template( 'archive-product.php' );
+
+//wc_get_template( 'archive-product.php' );
+
+?>
+
+
+<div class="col">
+<div class="page-header">
+  <h1>
+    <?php echo roots_title(); ?>
+  </h1>
+</div>
+  <div class="col-md-12">
+    <div class="row">
+      <div class="col-md-12">
+        <?php echo do_shortcode('[wpv-view name="STORE-MENU"]'); ?>
+      </div>
+    </div>
+      <?php echo do_shortcode('[wpv-view name="STORE-OTHER"]'); ?>
+  </div>
+</div>
