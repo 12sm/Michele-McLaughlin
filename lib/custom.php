bbc5a21af9514ce09668e2c7bb6552fac650dbf4
Ian Noyes <ian@12southmusic.com>
2014-11-03 17:24:36 -0600

    i have the worst syntax ever

--- lib/custom.php
+++ lib/custom.php
@@ -22,6 +22,6 @@ if ( $view_id == '68' ) {
   $ids = $wpdb->get_results( "SELECT meta_value FROM wp_postmeta WHERE meta_key = '_wpcf_belongs_awards-and-credits_id'" );
   $query['post__in'] = array($ids);
 }
-return $query
+return $query;
 }
 ?>
