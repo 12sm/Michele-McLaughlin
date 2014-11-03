9b6f67530dbdee99ac439032186725e658635ada
Ian Noyes <ian@12southmusic.com>
2014-11-03 15:39:54 -0600

    songbk template

---
+++ template-songbk.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Template Name: Songbook
+*/
+?>
+
+<?php get_template_part('templates/content', 'page-songbk'); ?>
---
+++ templates/content-page-songbk.php
@@ -0,0 +1,15 @@
+<div class="col">
+<div class="page-header">
+  <h1>
+    <?php echo roots_title(); ?>
+  </h1>
+</div>
+  <div class="col-md-12">
+    <div class="row">
+      <div class="col-md-12">
+        <?php echo do_shortcode('[wpv-view name="store-navigation"]'); ?>
+      </div>
+    </div>
+      <?php echo do_shortcode('[wpv-view name="STORE | SONGBOOKS"]'); ?>
+  </div>
+</div>c
