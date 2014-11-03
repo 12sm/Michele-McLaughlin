f8584f3eb294f657ac8261f7eca298682606fbbb
Ian Noyes <ian@12southmusic.com>
2014-11-02 21:45:31 -0600

    move it already

---
+++ woocommerce/archive-product.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The Template for displaying products in a product category. Simply includes the archive template.
+ *
+ * Override this template by copying it to yourtheme/woocommerce/taxonomy-product_cat.php
+ *
+ * @author    WooThemes
+ * @package   WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+
+//wc_get_template( 'archive-product.php' );
+echo "archive-product";
+?>
+
+
+<div class="col">
+<div class="page-header">
+  <h1>
+    <?php echo roots_title(); ?>
+  </h1>
+</div>
+  <div class="col-md-12">
+    <div class="row">
+      <div class="col-md-12">
+        <?php echo do_shortcode('[wpv-view name="store-navigation"]'); ?>
+      </div>
+    </div>
+      <?php echo do_shortcode('[wpv-view name="STORE-OTHER"]'); ?>
+  </div>
+</div>
