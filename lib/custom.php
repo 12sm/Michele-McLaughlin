dfcf23fac71da96cb3daf3e9b1fdfac29395c9d0
Ian Noyes <ian@12southmusic.com>
2014-11-03 18:05:40 -0600

    kill it

--- lib/custom.php
+++ lib/custom.php
@@ -13,15 +13,5 @@ function trim_shortcode($atts, $content = '') {
   return $content;
 }
 
-//filter by child posts
-add_filter('wpv_filter_query', 'parent_has_childs_func', 101, 3);
- 
-function parent_has_childs_func($query, $view_settings, $view_id) {
-if ( $view_id == '68' ) {
-  global $wpdb;
-  $ids = $wpdb->get_results( "SELECT meta_value FROM wp_postmeta WHERE meta_key = '_wpcf_belongs_albums_id'" );
-  $query['post__in'] = array($ids);
-}
-return $query;
-}
+
 ?>
