fc122fe8b76a9ce82db7faeffa7671b1ad8cda76
Ian Noyes <ian@12southmusic.com>
2014-11-05 13:58:12 -0600

    Revert "Revert "changing banners""
    This reverts commit 95353f0285a6f2cfaf27f6f8fa13c09446b63d75.

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,9 +1,11 @@
 <div class="max-wrap">
   <div class="row page-header">
-    <div class="banners col-md-12 col-lg-12">
-      <h1 class="bandit">
-        <?php echo roots_title(); ?>
-      </h1>
+    <div class="col-md-12 col-lg-12">
+      <div class="banners ">
+        <h1 class="bandit">
+          <?php echo roots_title(); ?>
+        </h1>
+      </div>
     </div>
   </div>
 </div>
