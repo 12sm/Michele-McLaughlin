7db66ad1fb7c1f275667cc310da8f02a36e2df57
Ian Noyes <ian@12southmusic.com>
2014-11-05 13:35:05 -0600

    js for single albums pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,6 +104,24 @@
          pagination: false,
          scrollPerPage : true
         });
+      },
+      single_albums: {
+        init: function(){
+          $('.play').on('click', playMusic);
+          $('.music-carousel').owlCarousel({
+            itemsCustom : [
+            [0, 2],
+            [480, 3],
+            [768, 4],
+            [1200, 4],
+            [1600, 4]
+            ],
+            navigation : true,
+            navigationText: ['<i class="fa fa-arrow-circle-left fa-3x"></i>','<i class="fa fa-arrow-circle-right fa-3x"></i>'],
+            pagination: false,
+            scrollPerPage : true
+          });
+        }
       }
     }
   };
