54aa41ac172c9f1f7b2bef1389c5a8af6d39ddef
Sarah <Sarah@12southmusic.com>
2014-11-06 12:11:56 -0600

    YESS

--- templates/content-page-songbk.php
+++ templates/content-page-songbk.php
@@ -4,11 +4,6 @@
   <?php get_template_part('templates/page', 'header'); ?>
 </div>
 
-<div class="page-header">
-  <h1>
-    <?php echo roots_title(); ?>
-  </h1>
-</div>
   <div class="col-md-12">
     <div class="row">
       <div class="col-md-12">
