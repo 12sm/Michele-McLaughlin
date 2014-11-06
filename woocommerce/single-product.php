69045fa70ce216eb74cb96f4b3d6c2c7e66cdafd
Ian Noyes <ian@12southmusic.com>
2014-11-06 16:07:59 -0600

    now

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,1 +1,17 @@
-<h1>Woocommerce is Poopy</h1>
+<?php while (have_posts()) : the_post(); ?>
+<div class="row col">
+<div class="col-md-12">
+  <article <?php post_class(); ?>>
+    <header>
+      <h1 class="entry-title"><?php the_title(); ?></h1>
+    </header>
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div>
+    <footer>
+      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+    </footer>
+  </article>
+</div>
+</div>
+<?php endwhile; ?>
