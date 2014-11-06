3896a29e3ea86994a3e88ef782531753aa326e78
Ian Noyes <ian@12southmusic.com>
2014-11-06 16:49:16 -0600

    forget you woocommerce!

--- woocommerce.php
+++ woocommerce.php
@@ -22,17 +22,11 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
       <?php if ( have_posts() ) : ?>
 
-        <?php woocommerce_product_loop_start(); ?>
-
-          <?php woocommerce_product_subcategories(); ?>
-
           <?php while ( have_posts() ) : the_post(); ?>
 
             <?php get_template_part( 'content', 'page' ); ?>
 
           <?php endwhile; // end of the loop. ?>
-
-        <?php woocommerce_product_loop_end(); ?>
 
       <?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
 
