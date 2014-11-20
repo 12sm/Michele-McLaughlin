e1092a02166f206fae1da24a2df1e19217e22374
michael <michael@12southmusic.com>
2014-11-20 00:34:15 -0600

    Product Attribute Shortcode

--- lib/custom.php
+++ lib/custom.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 define( 'UPLOADS', ''.'media' );
 
@@ -12,5 +12,142 @@ function trim_shortcode($atts, $content = '') {
   }
   return $content;
 }
+
+/**
+* WooCommerce: Show only one custom product attribute above Add-to-cart button on single product page.
+*/
+function woo_get_key_pa(){
+
+    // Edit below with the title of the attribute you wish to display
+    $desired_att = 'Key Signature';
+
+    global $product;
+    $attributes = $product->get_attributes();
+
+    if ( ! $attributes ) {
+        return;
+    }
+
+    $out = '';
+
+    foreach ( $attributes as $attribute ) {
+
+        if ( $attribute['is_taxonomy'] ) {
+
+            // sanitize the desired attribute into a taxonomy slug
+            $tax_slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '_', $desired_att)));
+
+            // if this is desired att, get value and label
+
+            if ( $attribute['name'] == 'pa_' . $tax_slug ) {
+
+                $terms = wp_get_post_terms( $product->id, $attribute['name'], 'all' );
+
+                // get the taxonomy
+                $tax = $terms[0]->taxonomy;
+
+                // get the tax object
+                $tax_object = get_taxonomy($tax);
+
+                // get tax label
+                if ( isset ($tax_object->labels->name) ) {
+                    $tax_label = $tax_object->labels->name;
+                } elseif ( isset( $tax_object->label ) ) {
+                    $tax_label = $tax_object->label;
+                }
+
+                foreach ( $terms as $term ) {
+
+                    $out .= $tax_label . ': ';
+                    $out .= $term->name . '<br />';
+
+                }
+
+            } // our desired att
+
+        } else {
+
+            // for atts which are NOT registered as taxonomies
+
+            // if this is desired att, get value and label
+            if ( $attribute['name'] == $desired_att ) {
+                $out .= $attribute['name'] . ': ';
+                $out .= $attribute['value'];
+            }
+        }
+
+
+    }
+
+    echo $out;
+}
+add_shortcode('product-att-key', 'isa_woo_get_key_pa');
+
+function woo_get_level_pa(){
+
+    // Edit below with the title of the attribute you wish to display
+    $desired_att = 'Level';
+
+    global $product;
+    $attributes = $product->get_attributes();
+
+    if ( ! $attributes ) {
+        return;
+    }
+
+    $out = '';
+
+    foreach ( $attributes as $attribute ) {
+
+        if ( $attribute['is_taxonomy'] ) {
+
+            // sanitize the desired attribute into a taxonomy slug
+            $tax_slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '_', $desired_att)));
+
+            // if this is desired att, get value and label
+
+            if ( $attribute['name'] == 'pa_' . $tax_slug ) {
+
+                $terms = wp_get_post_terms( $product->id, $attribute['name'], 'all' );
+
+                // get the taxonomy
+                $tax = $terms[0]->taxonomy;
+
+                // get the tax object
+                $tax_object = get_taxonomy($tax);
+
+                // get tax label
+                if ( isset ($tax_object->labels->name) ) {
+                    $tax_label = $tax_object->labels->name;
+                } elseif ( isset( $tax_object->label ) ) {
+                    $tax_label = $tax_object->label;
+                }
+
+                foreach ( $terms as $term ) {
+
+                    $out .= $tax_label . ': ';
+                    $out .= $term->name . '<br />';
+
+                }
+
+            } // our desired att
+
+        } else {
+
+            // for atts which are NOT registered as taxonomies
+
+            // if this is desired att, get value and label
+            if ( $attribute['name'] == $desired_att ) {
+                $out .= $attribute['name'] . ': ';
+                $out .= $attribute['value'];
+            }
+        }
+
+
+    }
+
+    echo $out;
+}
+add_shortcode('product-att-level', 'woo_get_level_pa');
 
 ?>
