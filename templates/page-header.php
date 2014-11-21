01822e51177624238cc0d5d41e68c76f00d1703f
Ian Noyes <ian@12southmusic.com>
2014-11-21 16:21:40 -0600

    change page header slightly

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,10 +1,12 @@
 <div class="max-wrap">
-  <div class="row page-header">
-    <div class="col-md-12 col-lg-12">
-      <div class="banners ">
-        <h1 class="bandit">
-          <?php echo roots_title(); ?>
-        </h1>
+  <div class="page-header">
+    <div class="row">
+      <div class="col-md-12 col-lg-12">
+        <div class="banners ">
+          <h1 class="bandit">
+            <?php echo roots_title(); ?>
+          </h1>
+        </div>
       </div>
     </div>
   </div>
