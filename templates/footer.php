055e14a67561b7038d92a1dd0e6fef90d89d87a6
michael <michael@12southmusic.com>
2014-11-21 13:15:48 -0600

    Add Michele GA

--- templates/footer.php
+++ templates/footer.php
@@ -177,17 +177,14 @@
 
 <?php wp_footer(); ?>
 
-<!-- Begin 12SM Network Analytics <!-->
-  <script type="text/javascript">
-	var _gaq = _gaq || [];
-  	_gaq.push(['_setAccount', 'UA-27814560-1']);
-  	_gaq.push(['_setDomainName', '12southmusic.com']);
-  	_gaq.push(['_setAllowLinker', true]);
-  	_gaq.push(['_trackPageview']);
- 	(function() {
-	  	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	  	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	  	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  	})();
-	</script>
-  <!-- End 12SM Network Analytics <!-->
+
+<script>
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-5080950-3']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
