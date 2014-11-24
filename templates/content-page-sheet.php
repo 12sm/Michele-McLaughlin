6d3e3a49f3e990c6342b65ea3ddc2bd50e78e5b2
Ian Noyes <ian@12southmusic.com>
2014-11-24 15:47:25 -0600

    Revert "Fix Store Nav Widths"
    This reverts commit 4e1da80cd4d4b6f88f0708230e8728dfffc049a5.

--- templates/content-page-sheet.php
+++ templates/content-page-sheet.php
@@ -1,14 +1,16 @@
-<div class="max-wrap">
-  <?php get_template_part('templates/page', 'header'); ?>
-</div>
-
-<div class="max-wrap">
-  <div class="col-md-12">
-    <div class="row">
-      <div class="col-md-12">
-        <?php echo do_shortcode('[wpv-view name="store-navigation"]'); ?>
+<div class="col">
+  <div class="max-wrap">
+    <?php get_template_part('templates/page', 'header'); ?>
+  </div>
+  
+  <div class="max-wrap">
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-12">
+          <?php echo do_shortcode('[wpv-view name="store-navigation"]'); ?>
+        </div>
       </div>
+        <?php echo do_shortcode('[wpv-view name="Sheet Music"]'); ?>
     </div>
-      <?php echo do_shortcode('[wpv-view name="Sheet Music"]'); ?>
   </div>
 </div>
