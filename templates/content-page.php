5e497459ba95bfb845804a1953cc4343b8d7c7cb
Sarah <Sarah@12southmusic.com>
2014-11-24 11:15:46 -0600

    max wrapping for licensing

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,6 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php the_content(); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
-<?php endwhile; ?>
+<div class="max-wrap">
+  <?php while (have_posts()) : the_post(); ?>
+    <?php the_content(); ?>
+    <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+  <?php endwhile; ?>
+</div>
