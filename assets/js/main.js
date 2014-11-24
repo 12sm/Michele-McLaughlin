d9dfc3f40f1e5963f2a22050a3a1151a47b47d5c
Ian Noyes <ian@12southmusic.com>
2014-11-24 17:07:43 -0600

    current instead of active

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@
         var nav = location.href.toLowerCase();
         var item  = $('.store-nav li a[href="' + nav  + '"]');
         if(item.length){
-          item.parent().addClass("active");
+          item.parent().addClass("current");
         }
 
         $('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
