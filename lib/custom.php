e4a17ffac0d2bee96517e51cbd1c9d77d3f56809
michael <michael@12southmusic.com>
2014-11-25 20:29:10 -0600

    sync encrypt

--- lib/custom.php
+++ lib/custom.php
@@ -19,7 +19,7 @@ function cort_shortcode($atts){
     ), $atts);
   $theurl = $a['url'];
   $theurl = substr($theurl, 35, 100);
-  $encrypted = openssl_encrypt($theurl, 'DES-CFB1', 'shooptie');
+  $encrypted = openssl_encrypt($theurl, 'BF-CBC', 'shooptie');
   $encoded = urlencode($encrypted);
   return $encoded;
 }
